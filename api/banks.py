c417cdafdcddbbfc85924582c4081ef582b9a303
mendozasho <shomendoza@gmail.com>
2023-08-24 13:59:17 -0700

    Added banks: 1. Wells Fargo 2. Best Buy 3. American Express

--- api/banks.py
+++ api/banks.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod, ABC
 from pathlib import Path
+from typing import Optional
 
 import pandas as pd
 
@@ -10,12 +11,22 @@ class Bank(ABC):
     _root_folder = config.REPORTS_FOLDER_FROM_ROOT
 
     @abstractmethod
-    def open_statement(self, statement_path: Path) -> pd.DataFrame:
+    def open_statement(self, statement_path: Path, *args, **kwargs) -> pd.DataFrame:
         """Should open the bank statement for the given bank
 
         :param statement_path: file path to the bank statement
         :return: the dataframe of the bank statement
         """
+
+
+class AmericanExpress(Bank):
+    def open_statement(self, statement_path: Path, *args, **kwargs) -> pd.DataFrame:
+        df = pd.read_csv(
+            statement_path,
+            *args,
+            **kwargs
+        )
+        return df
 
 
 class BestBuy(Bank):
@@ -29,4 +40,19 @@ class BestBuy(Bank):
         )
         df["amount"] = df["amount"].str.replace("$", "").astype(float).round(5)
         df["transaction_date"] = pd.to_datetime(df["transaction_date"]).dt.date
+        return df
+
+
+class WellsFargo(Bank):
+    def open_statement(self, statement_path: Path, tags: Optional[str] = None, *args, **kwargs) -> pd.DataFrame:
+        df = pd.read_csv(
+            statement_path,
+            header=None,
+            names=["transaction_date", "amount", "spacer", "spacer2", "transaction_name"],
+            *args, **kwargs
+        )
+        df["amount"] = df["amount"].astype(float).round(5)
+        df["transaction_date"] = pd.to_datetime(df["transaction_date"]).dt.date
+        df["transaction_type"] = [tags for _ in df["transaction_date"]]
+        df = df.drop(labels=["spacer", "spacer2"], axis="columns")
         return df
--- test/test_banks.py
+++ test/test_banks.py
@@ -1,7 +1,16 @@
 import pandas as pd
 
 import config
-from api.banks import BestBuy
+from api.banks import BestBuy, WellsFargo, AmericanExpress
+
+
+def test_amex():
+    path = config.REPORTS_FOLDER_FROM_ROOT / "activity.csv"
+    amex = AmericanExpress()
+    df = amex.open_statement(path)
+    print(df)
+    print(df.info())
+    print(df["Amount"].sum())
 
 
 def test_best_buy():
@@ -10,4 +19,22 @@ def test_best_buy():
     bb = BestBuy()
     df = bb.open_statement(path)
     print(df)
+    print(df["amount"].sum())
+
+
+def test_wells_fargo_cc():
+    path = config.REPORTS_FOLDER_FROM_ROOT / "CreditCard3 - 03.29.2023 - 06.27.2023.csv"
+    wf = WellsFargo()
+    df = wf.open_statement(path, tags="credit card")
+    print(df)
+    print(df.info())
+    print(df["amount"].sum())
+
+
+def test_wells_fargo_d():
+    path = config.REPORTS_FOLDER_FROM_ROOT / "Checking1 - 03.2023 - 06.27.2023.csv"
+    wf = WellsFargo()
+    df = wf.open_statement(path, tags="debit card")
+    print(df)
+    print(df.info())
     print(df["amount"].sum())
