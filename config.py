0aa95f6a2bfdd4206ba8da5ff31d49acaffcb2c7
mendozasho <shomendoza@gmail.com>
2023-10-13 19:25:12 -0700

    AUpdating bank and gsheet logic

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
 REPORTS_FOLDER_FROM_ROOT = Path(__file__).parent / "reports"
+CREDS = "/Users/shomendoza/Library/Mobile Documents/com~apple~CloudDocs/Developer/real-calc/credentials.json"
+WKB = "Bellywood Finance"
 
 print(REPORTS_FOLDER_FROM_ROOT)
--- gsheets.py
+++ gsheets.py
@@ -1,8 +1,13 @@
 import gspread
+import pandas as pd
 
 import config
 
 sa = gspread.oauth(credentials_filename=config.CREDS)
 sh = sa.open(config.WKB)
 
-wks = sh.worksheet()
+wks = sh.worksheet(title="Allocation")
+
+df = pd.DataFrame(wks.get_all_records())
+print(df)
+print(df.info())
